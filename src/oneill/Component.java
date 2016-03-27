3eed85df6dd76120d8e43d95c42de5ab8f93d5a9
lacens33 <lacens33@gmail.com>
2016-03-27 17:18:37 +0200

    Update Component.java

--- src/oneill/Component.java
+++ src/oneill/Component.java
@@ -5,21 +5,24 @@ public abstract class Component {
 	boolean steppable;
 	
 	Location getLocation()
-	{
+	{	
+		System.out.println("        "+this.getClass().getName()+": getLocation() -> "+this.location);
 		return location;
 	}
 	
 	void setLocation(Location value)
 	{
+		System.out.println("        "+this.getClass().getName()+": setLocation() -> "+value);
 		location = value;
 	}
 	
 	boolean getSteppable(){
-		
+		System.out.println("        "+this.getClass().getName()+": getStepable() -> "+this.stepable);
 		return steppable;
 	}
 	
 	void setSteppable(boolean b){
+		System.out.println("        "+this.getClass().getName()+": setStepable() -> "+b);
 		steppable = b;
 	}
 }
