fa6321f9af6851738e4c79064734077aaa8c195a
Peterta <peterta12@gmail.com>
2016-03-27 18:02:53 +0200

    hole

--- src/oneill/Hole.java
+++ src/oneill/Hole.java
@@ -5,7 +5,9 @@ public class Hole extends Component {
 	Location location;
 	Hole(Location loc){}
 	
-	void kill(Player p){}
+	void kill(Player p){
+		System.out.println("Get fucking killed m8");
+	}
 	void destroy(Box b){}
 	Location getLocation(){
 		return location;
